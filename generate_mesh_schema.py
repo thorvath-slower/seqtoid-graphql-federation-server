5a2573edfdda8963f1323afbb39fbcf9e9321aee
saq7 <saq7@users.noreply.github.com>
2023-11-20 14:37:30 -0500

    fix: updated all json schemas  (#22)
    Co-authored-by: Saquib Kothawala <saquib.kothawala@contractor.chanzuckerberg.com>

--- generate_mesh_schema.py
+++ generate_mesh_schema.py
@@ -1,6 +1,17 @@
 import json
 import sys
 
+types_map = {
+    "int": "integer",
+    "float": "number",
+    "str": "string",
+    "bool": "boolean",
+    "list": "array",
+    "dict": "object",
+    "object": "object",
+    None: "null"
+}
+
 def generate_property_schema(value):
     if isinstance(value, dict):
         return generate_mesh_schema(value)
@@ -10,7 +21,9 @@ def generate_property_schema(value):
             "items": generate_property_schema(value[0]) if value else { "type": "object" }
         }
     else:
-        return { "type": type(value).__name__ }
+        value_type = type(value).__name__
+        return { "type": types_map.get(value_type, value_type)}
+        # return { "type": "integer" if type(value).__name__ == "int" else type(value).__name__}
 
 def generate_mesh_schema(json_object):
     mesh_schema = {
@@ -39,6 +52,11 @@ def main():
     json_file_path = sys.argv[1]
     output_file_path = sys.argv[2]
     json_object = load_json_from_file(json_file_path)
+
+    # Check if the json_object is a list and take the first element
+    if isinstance(json_object, list) and json_object:
+        json_object = json_object[0]
+
     mesh_schema = generate_mesh_schema(json_object)
 
     if mesh_schema is not None:
