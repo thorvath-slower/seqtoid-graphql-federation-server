ec3f6d76c9401942973202489d2154b5c142eaaa
Jerry Fu <2072627+jfoo1984@users.noreply.github.com>
2024-01-30 09:25:31 -0800

    fix(ci): Pin happy stack eks to working version (#83)
    Co-authored-by: Jake Heath <76011913+jakeyheath@users.noreply.github.com>

--- .happy/terraform/envs/dev/main.tf
+++ .happy/terraform/envs/dev/main.tf
@@ -1,5 +1,5 @@
 module "stack" {
-  source           = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-eks?ref=main"
+  source           = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-eks?ref=happy-stack-eks-v4.27.1"
   image_tag        = var.image_tag
   image_tags       = jsondecode(var.image_tags)
   stack_name       = var.stack_name
