d49ce6c0ac1fea9b471494b2ec29753354ee2429
bchu1 <151689101+bchu1@users.noreply.github.com>
2024-03-18 16:49:10 -0700

    fix: Temporarily try seeing if fake paginating the aggregates query would solve the performance problem (#209)

--- utils/aggregateUtils.ts
+++ utils/aggregateUtils.ts
@@ -1,4 +1,7 @@
-import { fedWorkflowRunsAggregate, query_fedWorkflowRunsAggregate_aggregate_items } from "../.mesh";
+import {
+  fedWorkflowRunsAggregate,
+  query_fedWorkflowRunsAggregate_aggregate_items,
+} from "../.mesh";
 
 type RailsProjectAggregate = {
   id: number;
@@ -12,40 +15,52 @@ type RailsProjectAggregate = {
 // combine the aggregate counts from rails and next gen into a single response
 // that looks like a GraphQL aggregate query response
 export const processWorkflowsAggregateResponse = (
-  nextGenProjectAggregates: query_fedWorkflowRunsAggregate_aggregate_items[] | null,
-  railsProjects: RailsProjectAggregate[], 
-  nextGenEnabled: boolean
+  nextGenProjectAggregates:
+    | query_fedWorkflowRunsAggregate_aggregate_items[]
+    | null,
+  railsProjects: RailsProjectAggregate[],
+  nextGenEnabled: boolean,
 ): fedWorkflowRunsAggregate => {
   const aggregates: fedWorkflowRunsAggregate = {
-    "aggregate": [],
-  }
+    aggregate: [],
+  };
 
+  // TODO(bchu1): Remove this, just for temp testing!
+  let i = 0;
   for (const project of railsProjects) {
+    i++;
+    if (i >= 20) {
+      break;
+    }
     // TODO: enable more workflows coming from next gen
-    const nextGenCgCount = nextGenProjectAggregates?.find(
-      aggregate =>
-        aggregate?.groupBy?.workflowVersion?.workflow?.name ===
-          "consensus-genome" && aggregate?.groupBy?.collectionId === project.id,
-    )?.count || 0;
+    const nextGenCgCount =
+      nextGenProjectAggregates?.find(
+        aggregate =>
+          aggregate?.groupBy?.workflowVersion?.workflow?.name ===
+            "consensus-genome" &&
+          aggregate?.groupBy?.collectionId === project.id,
+      )?.count || 0;
     const counts = {
       "short-read-mngs": project.sample_counts.mngs_runs_count,
-      "consensus-genome": nextGenEnabled ? nextGenCgCount : project.sample_counts.cg_runs_count,
-      "amr": project.sample_counts.amr_runs_count,
-    }
+      "consensus-genome": nextGenEnabled
+        ? nextGenCgCount
+        : project.sample_counts.cg_runs_count,
+      amr: project.sample_counts.amr_runs_count,
+    };
     for (const workflow of ["consensus-genome", "short-read-mngs", "amr"]) {
       aggregates?.aggregate?.push({
-        "groupBy": {
-          "workflowVersion": {
-            "workflow": {
-              "name": workflow,
+        groupBy: {
+          workflowVersion: {
+            workflow: {
+              name: workflow,
             },
           },
-          "collectionId": project.id,
+          collectionId: project.id,
         },
-        "count": counts[workflow],
+        count: counts[workflow],
       });
     }
   }
 
   return aggregates;
-}
+};
