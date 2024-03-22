3a5b3fa6013fdd195040e6865dc507877e03a780
Suzette McCanny <smccanny@deloitte.ca>
2024-03-22 11:05:58 -0400

    fix: begin testing BulkDownloadCGOverview to nextGen (#227)

---
+++ resolver-functions/BulkDownloadsCGOverview.ts
@@ -0,0 +1,94 @@
+import { get, postWithCSRF, shouldReadFromNextGen } from "../utils/httpUtils";
+
+export const BulkDownloadsCGOverviewResolver = async (
+  root,
+  args,
+  context,
+  info,
+) => {
+  if (!args?.input) {
+    throw new Error("No input provided");
+  }
+  /* --------------------- Next Gen ------------------------- */
+  const nextGenEnabled = await shouldReadFromNextGen(context);
+  console.log("nextGenEnabled hello", nextGenEnabled);
+  if (nextGenEnabled) {
+    const entitiesQuery = `
+      query EntitiesQuery {
+        consensusGenomes(where: {producingRunId: {in: ${args?.input?.workflowRunIdsStrings}}}) {
+          metrics {
+            coverageDepth
+            totalReads
+            mappedReads
+            percentGenomeCalled
+            percentIdentity
+            referenceGenomeLength
+            gcPercent
+            refSnps
+            nMissing
+            nAmbiguous
+          }
+          sequencingRead {
+            sample {
+              name
+            }
+          }
+          referenceGenome {
+            name
+            id
+          }
+        }
+      } 
+    `;
+    const entitiesResp = await get({
+      args,
+      context,
+      serviceType: "entities",
+      customQuery: entitiesQuery,
+    });
+    console.log("entitiesResp", entitiesResp);
+    return entitiesResp;
+  }
+  const {
+    downloadType,
+    workflow,
+    includeMetadata,
+    workflowRunIds,
+    workflowRunIdsStrings,
+  } = args?.input;
+
+  //array of strings to array of numbers
+  const workflowRunIdsNumbers = workflowRunIdsStrings?.map(
+    id => id && parseInt(id),
+  );
+
+  const body = {
+    download_type: downloadType,
+    workflow: workflow,
+    params: {
+      include_metadata: { value: includeMetadata },
+      sample_ids: {
+        value: workflowRunIdsNumbers ? workflowRunIdsNumbers : workflowRunIds,
+      },
+      workflow: {
+        value: workflow,
+      },
+    },
+    workflow_run_ids: workflowRunIdsNumbers
+      ? workflowRunIdsNumbers
+      : workflowRunIds,
+  };
+  const res = await postWithCSRF({
+    url: `/bulk_downloads/consensus_genome_overview_data`,
+    body,
+    args,
+    context,
+  });
+  if (res?.cg_overview_rows) {
+    return {
+      cgOverviewRows: res?.cg_overview_rows,
+    };
+  } else {
+    throw new Error(res.error);
+  }
+};
--- resolvers.ts
+++ resolvers.ts
@@ -8,6 +8,7 @@ import {
   query_fedWorkflowRunsAggregate_aggregate_items,
   query_fedWorkflowRuns_items,
 } from "./.mesh";
+import { BulkDownloadsCGOverviewResolver } from "./resolver-functions/BulkDownloadsCGOverview";
 import { fedBulkDowloadsResolver } from "./resolver-functions/fedBulkDownloads";
 import { processWorkflowsAggregateResponse } from "./utils/aggregateUtils";
 import {
@@ -64,55 +65,7 @@ export const resolvers: Resolvers = {
       }, []);
     },
     fedBulkDownloads: fedBulkDowloadsResolver,
-    BulkDownloadCGOverview: async (root, args, context, info) => {
-      if (!args?.input) {
-        throw new Error("No input provided");
-      }
-      const {
-        downloadType,
-        workflow,
-        includeMetadata,
-        workflowRunIds,
-        workflowRunIdsStrings,
-      } = args?.input;
-
-      //array of strings to array of numbers
-      const workflowRunIdsNumbers = workflowRunIdsStrings?.map(
-        id => id && parseInt(id),
-      );
-
-      const body = {
-        download_type: downloadType,
-        workflow: workflow,
-        params: {
-          include_metadata: { value: includeMetadata },
-          sample_ids: {
-            value: workflowRunIdsNumbers
-              ? workflowRunIdsNumbers
-              : workflowRunIds,
-          },
-          workflow: {
-            value: workflow,
-          },
-        },
-        workflow_run_ids: workflowRunIdsNumbers
-          ? workflowRunIdsNumbers
-          : workflowRunIds,
-      };
-      const res = await postWithCSRF({
-        url: `/bulk_downloads/consensus_genome_overview_data`,
-        body,
-        args,
-        context,
-      });
-      if (res?.cg_overview_rows) {
-        return {
-          cgOverviewRows: res?.cg_overview_rows,
-        };
-      } else {
-        throw new Error(res.error);
-      }
-    },
+    BulkDownloadCGOverview: BulkDownloadsCGOverviewResolver,
     fedConsensusGenomes: async (root, args, context) => {
       /* --------------------- Next Gen ------------------------- */
       const nextGenEnabled = await shouldReadFromNextGen(context);
